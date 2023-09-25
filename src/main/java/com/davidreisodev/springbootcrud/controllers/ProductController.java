b3f195e1e9545d6f700150f0488e1f2a33609924
David Reis <eudavidreis.dev@gmail.com>
2023-09-25 16:28:48 -0300

    Update - Enable H2 console.

--- src/main/java/com/davidreisodev/springbootcrud/controllers/ProductController.java
+++ src/main/java/com/davidreisodev/springbootcrud/controllers/ProductController.java
@@ -1,15 +1,30 @@
 package com.davidreisodev.springbootcrud.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.davidreisodev.springbootcrud.models.ProductModel;
 import com.davidreisodev.springbootcrud.repositories.ProductRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
     
     @Autowired
     ProductRepository productRepository;
+
+    @PostMapping("/add_prod")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductModel product) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(product,productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 
 }
